21b28f8347bfd81da740056c29b3185528113849
vieteratoken-sys <vieteratoken@gmail.com>
2026-02-24 21:47:20 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const VTA_CONFIG = {
+    CONTRACT: "99hLf1BHfHKf2eUQbnwcaURf3kBqeqhAAdgV84jfiu8G",
+    NETWORK: "mainnet-beta",
+    RPC: "https://api.mainnet-beta.solana.com",
+    TOKEN_SYMBOL: "VTA"
+};
